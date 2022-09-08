938c85433a8b3dbf6cba303503627fd5f73d403e
Bezdomnyy <markovic.petar@gmail.com>
2022-09-08 15:24:48 -0700

    Tests

---
+++ build/testing/printing.d
@@ -0,0 +1,8 @@
+build/testing/printing.o: testing/printing.cpp testing/printing.hpp \
+ testing/../h/syscall_c.h testing/../h/../lib/hw.h
+
+testing/printing.hpp:
+
+testing/../h/syscall_c.h:
+
+testing/../h/../lib/hw.h:
